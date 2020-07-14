eab7c22d42ecef69252fd4d5abbeeb863218e9d1
JSH <tjdgus6024@naver.com>
2020-07-14 23:13:08 +0900

    Updates

---
+++ precache-manifest.638d5ed61b526ba36eb3447e79684154.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56d0f88b1484f984a0c0a5278e02f22",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "acb206959420247993fe",
+    "url": "/movie_app/static/css/main.7e3fa3bf.chunk.css"
+  },
+  {
+    "revision": "c0071c60e1a78cbbff2e",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.ce898f91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acb206959420247993fe",
+    "url": "/movie_app/static/js/main.5c66d638.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
